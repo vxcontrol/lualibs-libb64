5fb987db4cdf3ab247933b9f7280fa2e6c735114
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:49 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/libb64/build-osx32.sh
+++ csrc/libb64/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 *.c -shared -o ../../bin/osx32/libb64.dylib -Wall -I.
+gcc -arch i386 -O2 *.c -shared -install_name @loader_path/libb64.dylib -o ../../bin/osx32/libb64.dylib -Wall -I.
--- csrc/libb64/build-osx64.sh
+++ csrc/libb64/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 *.c -shared -o ../../bin/osx64/libb64.dylib -Wall -I.
+gcc -arch x86_64 -O2 *.c -shared -install_name @loader_path/libb64.dylib -o ../../bin/osx64/libb64.dylib -Wall -I.
