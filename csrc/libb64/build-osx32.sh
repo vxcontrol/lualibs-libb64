4d2d151f8cbe1a6acb8428f3222cc178817431a7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:02:39 +0200

    unimportant

--- csrc/libb64/build-osx32.sh
+++ csrc/libb64/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libb64.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libb64.dylib" \
 	D=libb64.dylib A=libb64.a ./build.sh
